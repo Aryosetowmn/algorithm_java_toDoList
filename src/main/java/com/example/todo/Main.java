36643a0f5d82e5607529b14abc09184a1fd8b8e6
HaikalCannavaro <haikalcannavaro64@gmail.com>
2026-05-20 20:15:06 +0700

    Menambahkan testing branch

--- src/main/java/com/example/todo/Main.java
+++ src/main/java/com/example/todo/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         TodoList list = new TodoList();
         list.addTask("Ngoding Java");
+        System.out.println("Testing branch fitur");
         System.out.println(list.getTasks().get(0).getDescription());
     }
 }
